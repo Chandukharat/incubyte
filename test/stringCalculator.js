75812cb7c5f3dc352377c72f9923c338dca70d45
Chandu Kharat <Kharatc187@gmail.com>
2024-06-20 13:40:58 +0530

    add function declared to get expected output when comma-separated numbers passed

---
+++ test/stringCalculator.js
@@ -0,0 +1,11 @@
+function add(numbers) {
+    if (numbers === "") {
+      return 0;
+    }
+    
+    const numberArray = numbers.split(',').map(Number);
+    return numberArray.reduce((sum, num) => sum + num, 0);
+  }
+  
+  module.exports = add;
+  
